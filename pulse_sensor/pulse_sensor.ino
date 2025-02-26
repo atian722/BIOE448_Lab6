ca47acf5e406c1ca203a00527c7191aad875358c
atian722 <ayt2@rice.edu>
2025-02-26 13:47:54 -0600

    adding initial flags and checkpoints

--- pulse_sensor/pulse_sensor.ino
+++ pulse_sensor/pulse_sensor.ino
@@ -1,6 +1,14 @@
-const int sensor_pin = 1;
+const int sensor_pin = 0;
 int pulse_signal = 0;
 float BPM = 0;
+
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+
+int upper_threshold = 1000;
+int lower_threshold = 5;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,4 +20,21 @@ void loop() {
   pulse_signal = analogRead(sensor_pin);
   Serial.println(pulse_signal);
   delay(50);
+
+  if (pulse_signal > upper_threshold && any_peak_detected ==
+  false) {
+  any_peak_detected = true;
+  // Do something about this peak
+  if (first_peak_detected == false) {
+    first_pulse_time = millis();
+  } 
+  else {
+    second_pulse_time = millis();
+    pulse_period = second_pulse_time - first_pulse_time;
+  }
+  }
+
+  if (pulse_signal < lower_threshold) {
+  any_peak_detected = false;
+  }
 }
