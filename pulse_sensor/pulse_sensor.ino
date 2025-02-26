fde6b7cc52ca0c0a8b6bf7e1a48df33946ab25e5
atian722 <ayt2@rice.edu>
2025-02-26 13:53:15 -0600

    added pulse_period to measure pulse timing

--- pulse_sensor/pulse_sensor.ino
+++ pulse_sensor/pulse_sensor.ino
@@ -10,6 +10,8 @@ unsigned long second_pulse_time = 0;
 int upper_threshold = 1000;
 int lower_threshold = 5;
 
+int pulse_period = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -18,20 +20,23 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   pulse_signal = analogRead(sensor_pin);
-  Serial.println(pulse_signal);
+  //Serial.println(pulse_signal);
   delay(50);
 
   if (pulse_signal > upper_threshold && any_peak_detected ==
   false) {
   any_peak_detected = true;
   // Do something about this peak
-  if (first_peak_detected == false) {
-    first_pulse_time = millis();
-  } 
-  else {
-    second_pulse_time = millis();
-    pulse_period = second_pulse_time - first_pulse_time;
-  }
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+      first_peak_detected = true;
+    } 
+    else {
+      second_pulse_time = millis();
+      pulse_period = second_pulse_time - first_pulse_time;
+      first_peak_detected = false;
+      Serial.println(pulse_period);
+    }
   }
 
   if (pulse_signal < lower_threshold) {
